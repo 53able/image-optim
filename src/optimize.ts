a2ca394949515621d810536346fefa44ca6bd92c
Go <go.tadano@algomatic.jp>
2024-09-11 14:49:59 +0900

    Optimize import statements for fs and path modules

--- src/optimize.ts
+++ src/optimize.ts
@@ -1,5 +1,5 @@
-import fs from 'fs';
-import path from 'path';
+import fs from 'node:fs';
+import path from 'node:path';
 import sharp from 'sharp';
 
 const SUPPORTED_EXTENSIONS = /\.(jpg|jpeg|png|gif)$/;
